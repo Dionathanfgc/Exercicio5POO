bcb5be126ad7a04c703064cd15f720ac6ac7ce7d
Dionathanfgc <100795356+Dionathanfgc@users.noreply.github.com>
2024-05-10 11:24:53 -0300

    Update SanduicheBuilder.ts

--- Ex5/src/builders/interfaces/SanduicheBuilder.ts
+++ Ex5/src/builders/interfaces/SanduicheBuilder.ts
@@ -4,4 +4,16 @@ export default class SanduicheBuilder implements IBuilder{
     reset(): void {
         this.sanduiche = new SanduicheBuilder();
     }
+    getSanduiche(): Sanduiche {
+        const result : Sanduiche = this.sanduiche;
+        this.reset();
+        return result;
+    }
+    setSanduicheType(value: SanduicheType) {
+        this.sanduiche.sanduicheType = value;
+    }
+    setBread
+    setProtein
+    setSalad
+    addSauce
 }
